991ea3aa9be4319864dbf75e96fc8ac2acba2ee8
Priyanka Saxena <saxena-priyanka@outlook.com>
2021-10-05 07:55:14 +1100

    Connected json file to index.html

---
+++ vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_1 = "departure.json";
+vegaEmbed("#australia_map", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
