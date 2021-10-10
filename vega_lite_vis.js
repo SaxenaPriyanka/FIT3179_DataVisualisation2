2f0690ee5b41cfe2e4540082a55a22b99b5b38aa
Priyanka Saxena <saxena-priyanka@outlook.com>
2021-10-10 11:13:21 +1100

    Name changes

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "departure.json";
+var vg_1 = "departure_map.json";
 vegaEmbed("#australia_map", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
