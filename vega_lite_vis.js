90491ca20a61e3dd8c341d1733c4b8d63530a81e
Priyanka Saxena <saxena-priyanka@outlook.com>
2021-10-10 19:05:36 +1100

    Line chart embedded in HTML page

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,8 @@
-var vg_1 = "departure_map.json";
-vegaEmbed("#australia_map", vg_1).then(function(result) {
- // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+var vg_1 = "arrival_map.json";
+var vg_2 = "arrival_numbers.json";
+vegaEmbed("#australia_arrival_map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   }).catch(console.error);
+vegaEmbed("#australia_arrival_line", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   }).catch(console.error);
