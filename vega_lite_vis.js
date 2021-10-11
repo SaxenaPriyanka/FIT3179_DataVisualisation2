6eaac7f83d2264383ecc35d6023744807b594128
Priyanka Saxena <saxena-priyanka@outlook.com>
2021-10-12 07:23:23 +1100

    line chart filtering

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,8 +1,16 @@
 var vg_1 = "arrival_map.json";
 var vg_2 = "arrival_numbers.json";
+var vg_3 = "arrival_numbers2.json";
+var vg_4 = "arrival_numbers3.json";
 vegaEmbed("#australia_arrival_map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
    }).catch(console.error);
 vegaEmbed("#australia_arrival_line", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   }).catch(console.error);
+vegaEmbed("#australia_arrival_line2", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   }).catch(console.error);
+vegaEmbed("#australia_arrival_line3", vg_4).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
    }).catch(console.error);
