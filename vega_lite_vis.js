a2c25a4f981922002e0e103f76d77eed9e487275
Priyanka Saxena <saxena-priyanka@outlook.com>
2021-10-17 04:51:04 +1100

    barchart added

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,12 +1,12 @@
 var vg_1 = "arrival_map.json";
 var vg_4 = "arrival_numbers3.json";
-var vg_5 = "barchart_arrival_countries.json";
+var vg_6 = "barchart.json";
 vegaEmbed("#australia_arrival_map", vg_1,{"actions":false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
    }).catch(console.error);
 vegaEmbed("#australia_arrival_line3", vg_4,{"actions":false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
    }).catch(console.error);
-   vegaEmbed("#countries_arrival_barchart", vg_5,{"actions":false}).then(function(result) {
+   vegaEmbed("#barchart_done", vg_6,{"actions":false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
    }).catch(console.error);
