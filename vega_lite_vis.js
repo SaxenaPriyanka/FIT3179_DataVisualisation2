1fbd9bea7c00afac04978b2c2d2676e60256e6f8
Priyanka Saxena <saxena-priyanka@outlook.com>
2021-10-18 00:22:25 +1100

    Adding time slider with map

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,20 +1,16 @@
-var vg_1 = "arrival_map.json";
-var vg_2 = "arrival_map2.json";
-var vg_4 = "line_chart_arrival.json";
-var vg_6 = "barchart_arrival_countries.json";
-var vg_7 = "barchart_visa.json";
-vegaEmbed("#australia_arrival_map", vg_1,{"actions":false}).then(function(result) {
+var vg_1 = "arrival_map2.json";
+var vg_2 = "line_chart_arrival.json";
+var vg_3 = "barchart_arrival_countries.json";
+var vg_4 = "barchart_visatypes.json";
+   vegaEmbed("#australia_arrival_map2", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
    }).catch(console.error);
-   vegaEmbed("#australia_arrival_map2", vg_2).then(function(result) {
+vegaEmbed("#australia_arrival_line", vg_2,{"actions":false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
    }).catch(console.error);
-vegaEmbed("#australia_arrival_line", vg_4,{"actions":false}).then(function(result) {
+   vegaEmbed("#barchart_arrivals_countries", vg_3,{"actions":false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
    }).catch(console.error);
-   vegaEmbed("#barchart_arrivals_countries", vg_6,{"actions":false}).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-   }).catch(console.error);
-   vegaEmbed("#barchart_arrivals_visa", vg_7,{"actions":false}).then(function(result) {
+   vegaEmbed("#barchart_arrivals_visa", vg_4,{"actions":false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
    }).catch(console.error);
