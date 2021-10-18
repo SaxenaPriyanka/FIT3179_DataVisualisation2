d27e5c8aa4c74c9dfdd19d6ceb78dc8d982f0b7c
Priyanka Saxena <saxena-priyanka@outlook.com>
2021-10-18 15:27:34 +1100

    Complete visualisation

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,7 +2,7 @@ var vg_1 = "map_arrivals.json";
 var vg_2 = "line_chart_arrival.json";
 var vg_3 = "barchart_arrival_countries.json";
 var vg_4 = "barchart_visatypes.json";
-   vegaEmbed("#australia_arrival_map2", vg_1,{"actions":false}).then(function(result) {
+   vegaEmbed("#australia_arrival_map", vg_1,{"actions":false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
    }).catch(console.error);
 vegaEmbed("#australia_arrival_line", vg_2,{"actions":false}).then(function(result) {
